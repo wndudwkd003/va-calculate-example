da8c64d181127be2db2a4fc21ed3c3537bc245c4
wndudwkd003 <ymail3@naver.com>
2023-05-03 23:05:32 +0900

    초기 커밋.

---
+++ Calculator_219124116/Form1.Designer.vb
@@ -0,0 +1,361 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button_t_3 = New System.Windows.Forms.Button()
+        Me.Button_t_4 = New System.Windows.Forms.Button()
+        Me.Button_t_2 = New System.Windows.Forms.Button()
+        Me.Button_t_1 = New System.Windows.Forms.Button()
+        Me.Button_t_5 = New System.Windows.Forms.Button()
+        Me.Button_t_6 = New System.Windows.Forms.Button()
+        Me.Button_t_7 = New System.Windows.Forms.Button()
+        Me.Button_t_8 = New System.Windows.Forms.Button()
+        Me.Button_n_9 = New System.Windows.Forms.Button()
+        Me.Button_n_6 = New System.Windows.Forms.Button()
+        Me.Button_n_3 = New System.Windows.Forms.Button()
+        Me.Button_t_result = New System.Windows.Forms.Button()
+        Me.Button_n_0 = New System.Windows.Forms.Button()
+        Me.Button_n_2 = New System.Windows.Forms.Button()
+        Me.Button_n_1 = New System.Windows.Forms.Button()
+        Me.Button_n_4 = New System.Windows.Forms.Button()
+        Me.Button_n_5 = New System.Windows.Forms.Button()
+        Me.Button_n_8 = New System.Windows.Forms.Button()
+        Me.Button_n_7 = New System.Windows.Forms.Button()
+        Me.Label_1 = New System.Windows.Forms.Label()
+        Me.Label_window = New System.Windows.Forms.Label()
+        Me.Label_2 = New System.Windows.Forms.Label()
+        Me.Label_3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button_t_3
+        '
+        Me.Button_t_3.Font = New System.Drawing.Font("휴먼둥근헤드라인", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button_t_3.Location = New System.Drawing.Point(196, 93)
+        Me.Button_t_3.Name = "Button_t_3"
+        Me.Button_t_3.Size = New System.Drawing.Size(80, 60)
+        Me.Button_t_3.TabIndex = 1
+        Me.Button_t_3.TabStop = False
+        Me.Button_t_3.Text = "C"
+        Me.Button_t_3.UseVisualStyleBackColor = True
+        '
+        'Button_t_4
+        '
+        Me.Button_t_4.Font = New System.Drawing.Font("휴먼둥근헤드라인", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button_t_4.Location = New System.Drawing.Point(282, 94)
+        Me.Button_t_4.Name = "Button_t_4"
+        Me.Button_t_4.Size = New System.Drawing.Size(80, 60)
+        Me.Button_t_4.TabIndex = 1
+        Me.Button_t_4.TabStop = False
+        Me.Button_t_4.Text = "<"
+        Me.Button_t_4.UseVisualStyleBackColor = True
+        '
+        'Button_t_2
+        '
+        Me.Button_t_2.Font = New System.Drawing.Font("휴먼둥근헤드라인", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button_t_2.Location = New System.Drawing.Point(110, 94)
+        Me.Button_t_2.Name = "Button_t_2"
+        Me.Button_t_2.Size = New System.Drawing.Size(80, 60)
+        Me.Button_t_2.TabIndex = 1
+        Me.Button_t_2.TabStop = False
+        Me.Button_t_2.Text = ")"
+        Me.Button_t_2.UseVisualStyleBackColor = True
+        '
+        'Button_t_1
+        '
+        Me.Button_t_1.Font = New System.Drawing.Font("휴먼둥근헤드라인", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button_t_1.Location = New System.Drawing.Point(22, 94)
+        Me.Button_t_1.Name = "Button_t_1"
+        Me.Button_t_1.Size = New System.Drawing.Size(80, 60)
+        Me.Button_t_1.TabIndex = 1
+        Me.Button_t_1.TabStop = False
+        Me.Button_t_1.Text = "("
+        Me.Button_t_1.UseVisualStyleBackColor = True
+        '
+        'Button_t_5
+        '
+        Me.Button_t_5.Font = New System.Drawing.Font("휴먼둥근헤드라인", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button_t_5.Location = New System.Drawing.Point(282, 160)
+        Me.Button_t_5.Name = "Button_t_5"
+        Me.Button_t_5.Size = New System.Drawing.Size(80, 60)
+        Me.Button_t_5.TabIndex = 1
+        Me.Button_t_5.TabStop = False
+        Me.Button_t_5.Text = "＋"
+        Me.Button_t_5.UseVisualStyleBackColor = True
+        '
+        'Button_t_6
+        '
+        Me.Button_t_6.Font = New System.Drawing.Font("휴먼둥근헤드라인", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button_t_6.Location = New System.Drawing.Point(282, 226)
+        Me.Button_t_6.Name = "Button_t_6"
+        Me.Button_t_6.Size = New System.Drawing.Size(80, 60)
+        Me.Button_t_6.TabIndex = 1
+        Me.Button_t_6.TabStop = False
+        Me.Button_t_6.Text = "-"
+        Me.Button_t_6.UseVisualStyleBackColor = True
+        '
+        'Button_t_7
+        '
+        Me.Button_t_7.Font = New System.Drawing.Font("휴먼둥근헤드라인", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button_t_7.Location = New System.Drawing.Point(282, 292)
+        Me.Button_t_7.Name = "Button_t_7"
+        Me.Button_t_7.Size = New System.Drawing.Size(80, 60)
+        Me.Button_t_7.TabIndex = 1
+        Me.Button_t_7.TabStop = False
+        Me.Button_t_7.Text = "×"
+        Me.Button_t_7.UseVisualStyleBackColor = True
+        '
+        'Button_t_8
+        '
+        Me.Button_t_8.Font = New System.Drawing.Font("휴먼둥근헤드라인", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button_t_8.Location = New System.Drawing.Point(282, 358)
+        Me.Button_t_8.Name = "Button_t_8"
+        Me.Button_t_8.Size = New System.Drawing.Size(80, 60)
+        Me.Button_t_8.TabIndex = 1
+        Me.Button_t_8.TabStop = False
+        Me.Button_t_8.Text = "÷"
+        Me.Button_t_8.UseVisualStyleBackColor = True
+        '
+        'Button_n_9
+        '
+        Me.Button_n_9.Font = New System.Drawing.Font("휴먼둥근헤드라인", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button_n_9.Location = New System.Drawing.Point(196, 160)
+        Me.Button_n_9.Name = "Button_n_9"
+        Me.Button_n_9.Size = New System.Drawing.Size(80, 60)
+        Me.Button_n_9.TabIndex = 1
+        Me.Button_n_9.TabStop = False
+        Me.Button_n_9.Text = "9"
+        Me.Button_n_9.UseVisualStyleBackColor = True
+        '
+        'Button_n_6
+        '
+        Me.Button_n_6.Font = New System.Drawing.Font("휴먼둥근헤드라인", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button_n_6.Location = New System.Drawing.Point(196, 226)
+        Me.Button_n_6.Name = "Button_n_6"
+        Me.Button_n_6.Size = New System.Drawing.Size(80, 60)
+        Me.Button_n_6.TabIndex = 1
+        Me.Button_n_6.TabStop = False
+        Me.Button_n_6.Text = "6"
+        Me.Button_n_6.UseVisualStyleBackColor = True
+        '
+        'Button_n_3
+        '
+        Me.Button_n_3.Font = New System.Drawing.Font("휴먼둥근헤드라인", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button_n_3.Location = New System.Drawing.Point(196, 292)
+        Me.Button_n_3.Name = "Button_n_3"
+        Me.Button_n_3.Size = New System.Drawing.Size(80, 60)
+        Me.Button_n_3.TabIndex = 1
+        Me.Button_n_3.TabStop = False
+        Me.Button_n_3.Text = "3"
+        Me.Button_n_3.UseVisualStyleBackColor = True
+        '
+        'Button_t_result
+        '
+        Me.Button_t_result.Font = New System.Drawing.Font("휴먼둥근헤드라인", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button_t_result.Location = New System.Drawing.Point(196, 358)
+        Me.Button_t_result.Name = "Button_t_result"
+        Me.Button_t_result.Size = New System.Drawing.Size(80, 60)
+        Me.Button_t_result.TabIndex = 1
+        Me.Button_t_result.TabStop = False
+        Me.Button_t_result.Text = "="
+        Me.Button_t_result.UseVisualStyleBackColor = True
+        '
+        'Button_n_0
+        '
+        Me.Button_n_0.Font = New System.Drawing.Font("휴먼둥근헤드라인", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button_n_0.Location = New System.Drawing.Point(24, 358)
+        Me.Button_n_0.Name = "Button_n_0"
+        Me.Button_n_0.Size = New System.Drawing.Size(164, 60)
+        Me.Button_n_0.TabIndex = 1
+        Me.Button_n_0.TabStop = False
+        Me.Button_n_0.Text = "0"
+        Me.Button_n_0.UseVisualStyleBackColor = True
+        '
+        'Button_n_2
+        '
+        Me.Button_n_2.Font = New System.Drawing.Font("휴먼둥근헤드라인", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button_n_2.Location = New System.Drawing.Point(110, 292)
+        Me.Button_n_2.Name = "Button_n_2"
+        Me.Button_n_2.Size = New System.Drawing.Size(80, 60)
+        Me.Button_n_2.TabIndex = 1
+        Me.Button_n_2.TabStop = False
+        Me.Button_n_2.Text = "2"
+        Me.Button_n_2.UseVisualStyleBackColor = True
+        '
+        'Button_n_1
+        '
+        Me.Button_n_1.Font = New System.Drawing.Font("휴먼둥근헤드라인", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button_n_1.Location = New System.Drawing.Point(24, 292)
+        Me.Button_n_1.Name = "Button_n_1"
+        Me.Button_n_1.Size = New System.Drawing.Size(80, 60)
+        Me.Button_n_1.TabIndex = 1
+        Me.Button_n_1.TabStop = False
+        Me.Button_n_1.Text = "1"
+        Me.Button_n_1.UseVisualStyleBackColor = True
+        '
+        'Button_n_4
+        '
+        Me.Button_n_4.Font = New System.Drawing.Font("휴먼둥근헤드라인", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button_n_4.Location = New System.Drawing.Point(22, 226)
+        Me.Button_n_4.Name = "Button_n_4"
+        Me.Button_n_4.Size = New System.Drawing.Size(80, 60)
+        Me.Button_n_4.TabIndex = 1
+        Me.Button_n_4.TabStop = False
+        Me.Button_n_4.Text = "4"
+        Me.Button_n_4.UseVisualStyleBackColor = True
+        '
+        'Button_n_5
+        '
+        Me.Button_n_5.Font = New System.Drawing.Font("휴먼둥근헤드라인", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button_n_5.Location = New System.Drawing.Point(108, 226)
+        Me.Button_n_5.Name = "Button_n_5"
+        Me.Button_n_5.Size = New System.Drawing.Size(80, 60)
+        Me.Button_n_5.TabIndex = 1
+        Me.Button_n_5.TabStop = False
+        Me.Button_n_5.Text = "5"
+        Me.Button_n_5.UseVisualStyleBackColor = True
+        '
+        'Button_n_8
+        '
+        Me.Button_n_8.Font = New System.Drawing.Font("휴먼둥근헤드라인", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button_n_8.Location = New System.Drawing.Point(108, 160)
+        Me.Button_n_8.Name = "Button_n_8"
+        Me.Button_n_8.Size = New System.Drawing.Size(80, 60)
+        Me.Button_n_8.TabIndex = 1
+        Me.Button_n_8.TabStop = False
+        Me.Button_n_8.Text = "8"
+        Me.Button_n_8.UseVisualStyleBackColor = True
+        '
+        'Button_n_7
+        '
+        Me.Button_n_7.Font = New System.Drawing.Font("휴먼둥근헤드라인", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button_n_7.Location = New System.Drawing.Point(22, 160)
+        Me.Button_n_7.Name = "Button_n_7"
+        Me.Button_n_7.Size = New System.Drawing.Size(80, 60)
+        Me.Button_n_7.TabIndex = 1
+        Me.Button_n_7.TabStop = False
+        Me.Button_n_7.Text = "7"
+        Me.Button_n_7.UseVisualStyleBackColor = True
+        '
+        'Label_1
+        '
+        Me.Label_1.AutoEllipsis = True
+        Me.Label_1.BackColor = System.Drawing.Color.White
+        Me.Label_1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label_1.Location = New System.Drawing.Point(391, 94)
+        Me.Label_1.Name = "Label_1"
+        Me.Label_1.Size = New System.Drawing.Size(288, 324)
+        Me.Label_1.TabIndex = 2
+        '
+        'Label_window
+        '
+        Me.Label_window.BackColor = System.Drawing.Color.White
+        Me.Label_window.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label_window.Font = New System.Drawing.Font("바탕", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label_window.Location = New System.Drawing.Point(22, 19)
+        Me.Label_window.Name = "Label_window"
+        Me.Label_window.Size = New System.Drawing.Size(340, 55)
+        Me.Label_window.TabIndex = 3
+        Me.Label_window.Tag = ""
+        Me.Label_window.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Label_2
+        '
+        Me.Label_2.AutoSize = True
+        Me.Label_2.Font = New System.Drawing.Font("바탕", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label_2.Location = New System.Drawing.Point(542, 59)
+        Me.Label_2.Name = "Label_2"
+        Me.Label_2.Size = New System.Drawing.Size(137, 15)
+        Me.Label_2.TabIndex = 4
+        Me.Label_2.Text = "후위 표기법 표시창"
+        Me.Label_2.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Label_3
+        '
+        Me.Label_3.AutoSize = True
+        Me.Label_3.Font = New System.Drawing.Font("바탕", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label_3.Location = New System.Drawing.Point(402, 19)
+        Me.Label_3.Name = "Label_3"
+        Me.Label_3.Size = New System.Drawing.Size(277, 15)
+        Me.Label_3.TabIndex = 4
+        Me.Label_3.Text = "간단한 연산을 할 수 있는 계산기 입니다"
+        Me.Label_3.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.MenuBar
+        Me.ClientSize = New System.Drawing.Size(704, 441)
+        Me.Controls.Add(Me.Label_3)
+        Me.Controls.Add(Me.Label_2)
+        Me.Controls.Add(Me.Label_window)
+        Me.Controls.Add(Me.Label_1)
+        Me.Controls.Add(Me.Button_t_1)
+        Me.Controls.Add(Me.Button_t_2)
+        Me.Controls.Add(Me.Button_t_8)
+        Me.Controls.Add(Me.Button_t_7)
+        Me.Controls.Add(Me.Button_t_6)
+        Me.Controls.Add(Me.Button_n_0)
+        Me.Controls.Add(Me.Button_t_result)
+        Me.Controls.Add(Me.Button_n_4)
+        Me.Controls.Add(Me.Button_n_1)
+        Me.Controls.Add(Me.Button_n_7)
+        Me.Controls.Add(Me.Button_n_8)
+        Me.Controls.Add(Me.Button_n_5)
+        Me.Controls.Add(Me.Button_n_2)
+        Me.Controls.Add(Me.Button_n_3)
+        Me.Controls.Add(Me.Button_n_6)
+        Me.Controls.Add(Me.Button_n_9)
+        Me.Controls.Add(Me.Button_t_5)
+        Me.Controls.Add(Me.Button_t_4)
+        Me.Controls.Add(Me.Button_t_3)
+        Me.Name = "Form1"
+        Me.Text = "사칙연산 계산기"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button_t_3 As Button
+    Friend WithEvents Button_t_4 As Button
+    Friend WithEvents Button_t_2 As Button
+    Friend WithEvents Button_t_1 As Button
+    Friend WithEvents Button_t_5 As Button
+    Friend WithEvents Button_t_6 As Button
+    Friend WithEvents Button_t_7 As Button
+    Friend WithEvents Button_t_8 As Button
+    Friend WithEvents Button_n_9 As Button
+    Friend WithEvents Button_n_6 As Button
+    Friend WithEvents Button_n_3 As Button
+    Friend WithEvents Button_t_result As Button
+    Friend WithEvents Button_n_0 As Button
+    Friend WithEvents Button_n_2 As Button
+    Friend WithEvents Button_n_1 As Button
+    Friend WithEvents Button_n_4 As Button
+    Friend WithEvents Button_n_5 As Button
+    Friend WithEvents Button_n_8 As Button
+    Friend WithEvents Button_n_7 As Button
+    Friend WithEvents Label_1 As Label
+    Friend WithEvents Label_window As Label
+    Friend WithEvents Label_2 As Label
+    Friend WithEvents Label_3 As Label
+End Class
---
+++ Calculator_219124116/Form1.vb
@@ -0,0 +1,291 @@
+﻿Imports Accessibility
+
+Public Class Form1
+    Public Structure Calculator
+        Dim numberArr As ArrayList
+        Dim signStack As Stack
+    End Structure
+
+    Dim bkCalulators As New ArrayList
+    Dim InputStringArr As New ArrayList
+    Dim InputString As String = ""
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim calculatorInstance As New Calculator With {
+            .numberArr = New ArrayList,
+            .signStack = New Stack
+        }
+        bkCalulators.Add(calculatorInstance)
+        Label_window.Text = InputString
+    End Sub
+
+    Private Sub Button_n_0_Click(sender As Object, e As EventArgs) Handles Button_n_0.Click
+        setLabelWindowIS("0")
+    End Sub
+
+    Private Sub setLabelWindowIS(v As String)
+        '입력 조건
+
+        '최대 길이 제한
+        If InputString.Length < 29 * 2 Then
+            If InputString.Length = 0 And Not IsNumeric(v) Then
+                '첫입력에 기호가 오면
+                If v = "(" Then
+                    InputString = InputString & v
+                Else
+                    Return
+                End If
+
+            ElseIf InputString.Length > 0 And Not IsNumeric(v) Then
+                If Not v = "(" And Not v = ")" And Not InputString.Last = "(" And Not InputString.Last = ")" And Not IsNumeric(InputString.Last) Then
+                    Dim tmp As String = ""
+                    Dim i As Integer
+
+                    '기호뒤에 기호가오면 맨끝 기호 대신 새로운 기호를 넣음
+                    For i = 0 To InputString.Length - 2 Step 1
+                        tmp = tmp & (InputString(i))
+                    Next i
+
+                    '기호 추가 전 입력된 문자열에 새로 입력된 기호 추가
+                    InputString = tmp & v
+                Else
+                    '그냥 기호 입력
+                    InputString = InputString & v
+                End If
+
+            Else
+                '문제 없으면 문자열 뒤에 추가
+                InputString = InputString & v
+            End If
+
+            Label_window.Text = InputString
+        Else
+            MsgBox("최대 58개만 입력할 수 있습니다. " + vbCrLf + "더이상 입력할 수 없습니다.", 0, "경고")
+        End If
+    End Sub
+
+    Private Sub Button_n_1_Click(sender As Object, e As EventArgs) Handles Button_n_1.Click
+        setLabelWindowIS("1")
+    End Sub
+
+    Private Sub Button_n_2_Click(sender As Object, e As EventArgs) Handles Button_n_2.Click
+        setLabelWindowIS("2")
+    End Sub
+
+    Private Sub Button_n_3_Click(sender As Object, e As EventArgs) Handles Button_n_3.Click
+        setLabelWindowIS("3")
+    End Sub
+
+    Private Sub Button_n_4_Click(sender As Object, e As EventArgs) Handles Button_n_4.Click
+        setLabelWindowIS("4")
+    End Sub
+
+    Private Sub Button_n_5_Click(sender As Object, e As EventArgs) Handles Button_n_5.Click
+        setLabelWindowIS("5")
+    End Sub
+
+    Private Sub Button_n_6_Click(sender As Object, e As EventArgs) Handles Button_n_6.Click
+        setLabelWindowIS("6")
+    End Sub
+
+    Private Sub Button_n_7_Click(sender As Object, e As EventArgs) Handles Button_n_7.Click
+        setLabelWindowIS("7")
+    End Sub
+
+    Private Sub Button_n_8_Click(sender As Object, e As EventArgs) Handles Button_n_8.Click
+        setLabelWindowIS("8")
+    End Sub
+
+    Private Sub Button_n_9_Click(sender As Object, e As EventArgs) Handles Button_n_9.Click
+        setLabelWindowIS("9")
+    End Sub
+
+    Private Sub Button_t_3_Click(sender As Object, e As EventArgs) Handles Button_t_3.Click
+        InputStringArr.Clear()
+        InputString = ""
+        Label_window.Text = InputString
+        Label_1.Text = InputString
+    End Sub
+
+    Private Sub Button_t_4_Click(sender As Object, e As EventArgs) Handles Button_t_4.Click
+        If InputString.Length > 0 Then
+            InputString = InputString.Remove(InputString.Length - 1)
+            Label_window.Text = InputString
+        Else
+            Label_window.Text = InputString
+        End If
+    End Sub
+
+    Private Sub Button_t_1_Click(sender As Object, e As EventArgs) Handles Button_t_1.Click
+        setLabelWindowIS("(")
+    End Sub
+
+    Private Sub Button_t_2_Click(sender As Object, e As EventArgs) Handles Button_t_2.Click
+        setLabelWindowIS(")")
+    End Sub
+
+    Private Sub Button_t_5_Click(sender As Object, e As EventArgs) Handles Button_t_5.Click
+        setLabelWindowIS("+")
+    End Sub
+
+    Private Sub Button_t_6_Click(sender As Object, e As EventArgs) Handles Button_t_6.Click
+        setLabelWindowIS("-")
+    End Sub
+
+    Private Sub Button_t_7_Click(sender As Object, e As EventArgs) Handles Button_t_7.Click
+        setLabelWindowIS("×")
+    End Sub
+
+    Private Sub Button_t_8_Click(sender As Object, e As EventArgs) Handles Button_t_8.Click
+        setLabelWindowIS("÷")
+    End Sub
+    Private Sub setNumberArrFromStack(numberArr As ArrayList, signStack As Stack)
+        Dim i As Integer
+        For i = 0 To signStack.Count - 1 Step 1
+            numberArr.Add(signStack.Pop)
+        Next i
+    End Sub
+
+
+    Private Sub Button_t_result_Click(sender As Object, e As EventArgs) Handles Button_t_result.Click
+        '일단 기본적인것만 우선 오류제어
+        If InputString = "" Then
+            Return
+        ElseIf Not IsNumeric(InputString(InputString.Length - 1)) Then
+            Select Case InputString(InputString.Length - 1)
+                Case "+"
+                    Return
+                Case "-"
+                    Return
+                Case "×"
+                    Return
+                Case "÷"
+                    Return
+            End Select
+        End If
+
+        '입력된 식 분리하기
+        setInputStringArr()
+
+        '원본 계산식 저장 생성
+        Dim openBracket As Integer = 0
+
+        '후위 표기법 변환
+        Dim i As Integer
+        For i = 0 To InputStringArr.Count - 1 Step 1
+            If IsNumeric(InputStringArr(i)) Then
+                ' 숫자면 그냥 넣음
+                bkCalulators(openBracket).numberArr.Add(InputStringArr(i))
+
+            Else
+                '기호면 몇개 체크해야함
+                If InputStringArr(i) = "(" Then
+                    '괄호가 열리면 
+                    openBracket += 1
+                    Dim calculatorInstance As New Calculator With {
+                        .numberArr = New ArrayList,
+                        .signStack = New Stack
+                    }
+                    bkCalulators.Add(calculatorInstance)
+
+                ElseIf InputStringArr(i) = ")" Then
+                    '괄호가 닫히면 삭제
+                    setNumberArrFromStack(bkCalulators(openBracket).numberArr, bkCalulators(openBracket).signStack)
+                    '이전 배열로 다 추가
+                    Dim u As Integer
+                    For u = 0 To bkCalulators(openBracket).numberArr.Count - 1 Step 1
+                        bkCalulators(openBracket - 1).numberArr.Add(bkCalulators(openBracket).numberArr(u))
+                    Next u
+                    bkCalulators.RemoveAt(openBracket)
+                    openBracket -= 1
+
+                ElseIf bkCalulators(openBracket).signStack.Count = 0 And (InputStringArr(i) = "+" Or InputStringArr(i) = "-") Then
+                    '스택의 처음 +-이런건 그냥 넣음
+                    bkCalulators(openBracket).signStack.Push(InputStringArr(i))
+
+                ElseIf InputStringArr(i) = "+" Or InputStringArr(i) = "-" Then
+                    '그다음 +-는 스택이랑 교환
+                    setNumberArrFromStack(bkCalulators(openBracket).numberArr, bkCalulators(openBracket).signStack)
+                    bkCalulators(openBracket).signStack.Clear()
+                    bkCalulators(openBracket).signStack.Push(InputStringArr(i))
+
+                ElseIf InputStringArr(i) = "×" Or InputStringArr(i) = "÷" Then
+                    '*/는 스택에 넣음
+                    bkCalulators(openBracket).signStack.Push(InputStringArr(i))
+
+                End If
+            End If
+        Next i
+
+        '마지막 스택에 있는거 다넣음
+        setNumberArrFromStack(bkCalulators(openBracket).numberArr, bkCalulators(openBracket).signStack)
+
+        '후위연산 출력
+        Dim tmp As String = ""
+        For i = 0 To bkCalulators(openBracket).numberArr.Count - 1 Step 1
+            tmp = tmp & bkCalulators(openBracket).numberArr(i)
+        Next
+        Label_1.Text = tmp
+
+        '실제 계산
+        '늦은바인딩에러떠서 배열을 새로만들어야함
+        Dim result As New ArrayList
+        result = setResultNumberArr(bkCalulators(openBracket).numberArr)
+        Label_window.Text = CStr(result(0))
+
+        bkCalulators(openBracket).numberArr.Clear()
+        bkCalulators(openBracket).signStack.Clear()
+        result.Clear()
+        InputStringArr.Clear()
+        InputString = ""
+
+    End Sub
+
+    Private Function setResultNumberArr(numberArr As ArrayList) As ArrayList
+        '계산해서 배열 반환
+        Dim tmp As Integer = 0
+        Dim i As Integer = 0
+
+        While Not numberArr.Count = 1
+            If Not IsNumeric(numberArr(i)) Then
+
+                Select Case numberArr(i)
+                    Case "+"
+                        tmp = CInt(numberArr(i - 2)) + CInt(numberArr(i - 1))
+                    Case "-"
+                        tmp = CInt(numberArr(i - 2)) - CInt(numberArr(i - 1))
+                    Case "×"
+                        tmp = CInt(numberArr(i - 2)) * CInt(numberArr(i - 1))
+                    Case "÷"
+                        tmp = CInt(numberArr(i - 2)) \ CInt(numberArr(i - 1))
+
+                End Select
+
+                numberArr(i - 2) = tmp
+                numberArr.RemoveAt(i)
+                numberArr.RemoveAt(i - 1)
+
+                numberArr = setResultNumberArr(numberArr)
+            End If
+
+            i += 1
+        End While
+        Return numberArr
+    End Function
+
+    Private Sub setInputStringArr()
+        '분리하기
+        Dim tmp As String = ""
+        Dim i As Integer
+        For i = 0 To InputString.Length - 1 Step 1
+            If Not IsNumeric(InputString(i)) Then
+                InputStringArr.Add(tmp)
+                tmp = ""
+                InputStringArr.Add(InputString(i))
+            Else
+                tmp = tmp & InputString(i)
+            End If
+        Next i
+        InputStringArr.Add(tmp)
+    End Sub
+End Class
